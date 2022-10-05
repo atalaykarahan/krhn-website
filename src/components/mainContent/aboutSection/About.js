43987d4bf1eb02f68d6656b825acb6e1880e7ccf
atalaykarahan <krhnatalay@gmail.com>
2022-10-05 17:33:21 +0300

    parallax kısmı olabildiğince benzetildi

--- src/components/mainContent/aboutSection/About.js
+++ src/components/mainContent/aboutSection/About.js
@@ -1,15 +1,25 @@
 import React from "react";
-import {Parallax} from 'react-parallax';
+import { Parallax } from "react-parallax";
 import bigPlaceholder from "../../../images/bigPlaceHolder.jpg";
-export default function About() {
 
+export default function About() {
   return (
     <div className="index-section-flush">
-       <Parallax bgImage={bigPlaceholder}>
-        <div style={{ height: 550 }}>
-        <div> Test yazısı buraya geleckek hadi bakalım</div>
+      <Parallax
+        bgImage={bigPlaceholder}
+        className="parallax-container"
+        strength={-150}
+      >
+        <div className="black-card">
+          <p>Hakkında</p>
+          <h2>Atalay</h2>
+          <div>
+            As a life-long creator I know how challenging the editing process
+            can be. That's why I decided to build tools which help you level up
+            your work while saving time along the way.
+          </div>
         </div>
-       </Parallax>
+      </Parallax>
     </div>
   );
 }
