f44e438963f346e9c357610533255712ba60005f
atalaykarahan <krhnatalay@gmail.com>
2022-10-06 20:21:08 +0300

    template işlemine başlamadan hemen önce

--- src/root/App.js
+++ src/root/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./Dashboard";
 import MenuStatik from "../components/pages/statik/menu/MenuStatik";
 import OthersStatik from "../components/pages/statik/others/OthersStatik";
 import PortfoyStatik from "../components/pages/statik/portfoy/PortfoyStatik";
+import StatikTemplate from "../components/pages/statik/StatikTemplate";
 import Footer from "../components/footer/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductsContextProvider from "../context/ProductsContext";
@@ -22,6 +23,7 @@ function App() {
             <Route path="statik/menu/" element={<MenuStatik />} />
             <Route path="statik/portfoy/" element={<PortfoyStatik />} />
             <Route path="statik/others/" element={<OthersStatik />} />
+            <Route path="statik/template/" element={<StatikTemplate />} />
           </Routes>
         </ProductsContextProvider>
         <Footer />
