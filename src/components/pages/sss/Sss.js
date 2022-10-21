51b60206bc6a5882ca4244ef77d975140de2d954
Atalay Karahan <krhnatalay@gmail.com>
2022-10-21 12:30:33 +0300

    SSS template kısmı yarım kaldı devam edicek

--- src/components/pages/sss/Sss.js
+++ src/components/pages/sss/Sss.js
@@ -1,24 +1,27 @@
 import React from "react";
 import pencelImage from "../../../images/sss/pencel.png";
+import Accordion from "react-bootstrap/Accordion";
 
 export default function Sss() {
   const SssText = [
     {
       icon: pencelImage,
+      key: 0,
       header: "Fiyatlar neye göre belirlenmekte",
       text: "Web tasarım fiyatları; İşlenecek olan içerik, sayfa sayısı, Yazılacak olan web site yazılımı web site içinde kaç dil olacağı SSL olup olmama durumu ve çeşitli özel formlara göre fiyatlandırılmaktadır.",
     },
     {
-      icon: "test",
+      icon: pencelImage,
+      key: 1,
       header: "Web hosting ve domain seçimini siz mi yapıyorsunuz",
       text: "Evet. Sizin yerinize web hosting ve domain seçimi yaparak gerekli tescil işlemlerini gerçekleştirebiliyoruz. Aylık ve birkaç yıllık şekilde yapılabilmektedir. Zamanı geldiğinde gerekli uzatma işlemlerini sizin yerinize de gerçekleştirebilmekteyiz.",
     },
     {
-      icon: "test",
+      icon: pencelImage,
+      key: 2,
       header: "Web sitemi kendim yönetebilir miyim",
       text: "Yaptığımı projeler için içeriğe uygun olarak bir yönetim paneli oluşturmaktayız. Bu yönetim paneli yardımıyla web site üzerinde gerekli değişiklikleri yapabilirsiniz. Yönetim paneline giriş yaparak bize ihtiyaç duymadan yeni güncellemeler yapmanız mümkündür. Eğer bu tip güncelleme işlemlerini bizim takip etmemizi istiyorsanız tüm bunları sizin yerinize biz yapıyoruz.",
     },
-    { icon: "test", header: "deneme", text: "deneme" },
   ];
 
   return (
@@ -33,139 +36,23 @@ export default function Sss() {
 
       {/* ========= Soruların sıralandığı bölüm ========= */}
 
-      <ul id="accordion">
-        {/* ========= ilk liste  ========= */}
-        <li>
-          <div className="d-flex justify-content-between align-items-center">
-            {/* ========= Resim ========= */}
-            <div className="d-flex">
-              <img
-                className="img-fluid"
-                alt="ikon-resmi"
-                src={SssText[0].icon}
-              />
-            </div>
-
-            {/* ========= Başlık ========= */}
-            <div className="d-flex pt-2">
-              <label htmlFor="first" className="template-section_header">
-                <h2>{SssText[0].header}</h2>
-              </label>
-              <input type={"radio"} name="accordion" id="first" checked={true} />
-            </div>
-
-            {/* ========= Arrow ikonu ========= */}
-            <div className="d-flex pe-2">
-              <svg
-                focusable="false"
-                viewBox="0 0 28 16"
-                className="icon-chevron-down"
-              >
-                <path
-                  d="M1.57 1.59l12.76 12.77L27.1 1.59"
-                  strokeWidth="2"
-                  stroke="#000"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </div>
-          </div>
-
-          {/* ========= Açıklama yazısı ========= */}
-          <div className="content">
-            <p>{SssText[0].text}</p>
-          </div>
-        </li>
-
-        {/* ========= ikinci liste ========= */}
-        <li>
-          <div className="d-flex justify-content-between align-items-center">
-            {/* ========= Resim ========= */}
-            <div className="d-flex">
-              <img
-                className="img-fluid"
-                alt="ikon-resmi"
-                src={SssText[0].icon}
-              />
-            </div>
-
-            {/* ========= Başlık ========= */}
-            <div className="d-flex pt-2">
-              <label htmlFor="second" className="template-section_header">
-                <h2>{SssText[0].header}</h2>
-              </label>
-              <input type={"radio"} name="accordion" id="second" />
-            </div>
-
-            {/* ========= Arrow ikonu ========= */}
-            <div className="d-flex pe-2">
-              <svg
-                focusable="false"
-                viewBox="0 0 28 16"
-                className="icon-chevron-down"
-              >
-                <path
-                  d="M1.57 1.59l12.76 12.77L27.1 1.59"
-                  strokeWidth="2"
-                  stroke="#000"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </div>
-          </div>
-
-          {/* ========= Açıklama yazısı ========= */}
-          <div className="content">
-            <p>{SssText[0].text}</p>
-          </div>
-        </li>
-
-        {/* ========= üçüncü liste ========= */}
-        <li>
-          <div className="d-flex justify-content-between align-items-center">
-            {/* ========= Resim ========= */}
-            <div className="d-flex">
-              <img
-                className="img-fluid"
-                alt="ikon-resmi"
-                src={SssText[0].icon}
-              />
-            </div>
-
-            {/* ========= Başlık ========= */}
-            <div className="d-flex pt-2">
-              <label htmlFor="third" className="template-section_header">
-                <h2>{SssText[0].header}</h2>
-              </label>
-              <input type={"radio"} name="accordion" id="third" />
-            </div>
-
-            {/* ========= Arrow ikonu ========= */}
-            <div className="d-flex pe-2">
-              <svg
-                focusable="false"
-                viewBox="0 0 28 16"
-                className="icon-chevron-down"
-              >
-                <path
-                  d="M1.57 1.59l12.76 12.77L27.1 1.59"
-                  strokeWidth="2"
-                  stroke="#000"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </div>
-          </div>
-
-          {/* ========= Açıklama yazısı ========= */}
-          <div className="content">
-            <p>{SssText[0].text}</p>
-          </div>
-        </li>
-      </ul>
+      <Accordion bsPrefix="krhn-accordion" defaultActiveKey="0">
+        {SssText.map((subject) => (
+          <Accordion.Item
+            bsPrefix="accordion-item d-flex align-items-center"
+            eventKey={subject.key}
+            key={subject.key}
+          >
+            <Accordion.Header>
+              <img alt="ikon resmi" src={subject.icon} />
+              <div className="container-item_header">
+                <h2>{subject.header}</h2>
+              </div>
+            </Accordion.Header>
+            <Accordion.Body>{subject.text}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
     </div>
   );
 }
