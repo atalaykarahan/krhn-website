876fc61d433b06dff3c011b553a5e0ea3f0996b3
krhnatalay <krhnatalay@gmail.com>
2022-10-22 18:20:16 +0300

    SSS sayfa kısmı komple bitti geriye yazıların düzenlenmesi kaldı

--- src/components/pages/sss/Sss.js
+++ src/components/pages/sss/Sss.js
@@ -1,23 +1,19 @@
 import React from "react";
-import pencelImage from "../../../images/sss/pencel.png";
 import Accordion from "react-bootstrap/Accordion";
 
 export default function Sss() {
   const SssText = [
     {
-      icon: pencelImage,
       key: 0,
       header: "Fiyatlar neye göre belirlenmekte",
       text: "Web tasarım fiyatları; İşlenecek olan içerik, sayfa sayısı, Yazılacak olan web site yazılımı web site içinde kaç dil olacağı SSL olup olmama durumu ve çeşitli özel formlara göre fiyatlandırılmaktadır.",
     },
     {
-      icon: pencelImage,
       key: 1,
       header: "Web hosting ve domain seçimini siz mi yapıyorsunuz",
       text: "Evet. Sizin yerinize web hosting ve domain seçimi yaparak gerekli tescil işlemlerini gerçekleştirebiliyoruz. Aylık ve birkaç yıllık şekilde yapılabilmektedir. Zamanı geldiğinde gerekli uzatma işlemlerini sizin yerinize de gerçekleştirebilmekteyiz.",
     },
     {
-      icon: pencelImage,
       key: 2,
       header: "Web sitemi kendim yönetebilir miyim",
       text: "Yaptığımı projeler için içeriğe uygun olarak bir yönetim paneli oluşturmaktayız. Bu yönetim paneli yardımıyla web site üzerinde gerekli değişiklikleri yapabilirsiniz. Yönetim paneline giriş yaparak bize ihtiyaç duymadan yeni güncellemeler yapmanız mümkündür. Eğer bu tip güncelleme işlemlerini bizim takip etmemizi istiyorsanız tüm bunları sizin yerinize biz yapıyoruz.",
@@ -36,19 +32,34 @@ export default function Sss() {
 
       {/* ========= Soruların sıralandığı bölüm ========= */}
 
-      <Accordion bsPrefix="krhn-accordion" defaultActiveKey="0">
+      <Accordion bsPrefix="krhn-accordion" defaultActiveKey={0}>
         {SssText.map((subject) => (
           <Accordion.Item
-            bsPrefix="accordion-item d-flex align-items-center"
+            bsPrefix="accordion-item text-center"
             eventKey={subject.key}
             key={subject.key}
           >
             <Accordion.Header>
-              <img alt="ikon resmi" src={subject.icon} />
-              <div className="container-item_header">
+              <div className="krhn-baslik_ortala">
                 <h2>{subject.header}</h2>
               </div>
+              <div id="krhn-arrow">
+                <svg
+                  focusable="false"
+                  viewBox="0 0 28 16"
+                  className="icon-chevron-down"
+                >
+                  <path
+                    d="M1.57 1.59l12.76 12.77L27.1 1.59"
+                    strokeWidth="2"
+                    stroke="#000"
+                    fill="none"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
             </Accordion.Header>
+
             <Accordion.Body>{subject.text}</Accordion.Body>
           </Accordion.Item>
         ))}
